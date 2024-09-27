10a9ecba799e126a4ea6f958742ac291f9b90609
Stefano Di Pasquale <stefaniscion@gmail.com>
2024-09-27 04:07:50 +0200

    added text elaboration (removing links, removing bolds)

--- merger.py
+++ merger.py
@@ -1,16 +1,16 @@
 import argparse
+import re
 from pathlib import Path
+
 
 def get_file_content(file_path: Path):
     with open(file_path, 'r') as file_handler:
         return file_handler.read()
 
+
 parser = argparse.ArgumentParser(description='Merges a folder of markdowns in a single markdown.')
 parser.add_argument(
-    'path', 
-    metavar='D', 
-    type=Path, 
-    help='path of directory containing the markdowns to be merged'
+    'path', metavar='D', type=Path, help='path of directory containing the markdowns to be merged'
 )
 args = parser.parse_args()
 
@@ -22,6 +22,15 @@ if args.path.is_dir():
         if file_path.suffix == '.md':
             file_content = get_file_content(file_path)
             file_content += "\n\n"
+
+            # remove bolds
+            bold_re = r"\*\*([^\*]+)\*\*"
+            file_content = re.sub(bold_re, r"\1", file_content)
+
+            # replace markdown links with link text
+            md_link_re = r"\[([^\]]+)\]\(([^)]+)\)"
+            file_content = re.sub(md_link_re, "**" + r"\1" + "**", file_content)
+
             if file_content:
                 output_handler.write(file_content)
 else:
